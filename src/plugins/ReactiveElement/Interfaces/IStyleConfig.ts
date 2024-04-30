17b839256b15c00adcbb95b7fb3f4dfe8d58b26a
Developer <130534010+ghost160111@users.noreply.github.com>
2024-04-30 16:21:10 +0500

    added some changes

--- src/plugins/ReactiveElement/Interfaces/IStyleConfig.ts
+++
@@ -1,8 +0,0 @@
-export default interface IStyleConfig {
-  adds?: {
-    margins?: boolean;
-  }
-  css?: string;
-  sass?: string;
-  setOpacityAnimation?: boolean;
-}
--- src/plugins/ReactiveElement/Utils/StringMethods.ts
+++
@@ -1,8 +0,0 @@
-export class StringMethods {
-  static setOneLineText(text: string) {
-    if (typeof text === "string") {
-      const oneLineText: string = text.replace(/\r/g, "").replace(/\n/g, "");
-      return oneLineText;
-    }
-  }
-}
